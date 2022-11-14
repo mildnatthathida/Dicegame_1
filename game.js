c193423a944c09dd973370c0f8f514aeaad83253
mildnatthathida <mildnatthathida@gmail.com>
2022-11-14 18:52:39 +0700

    game

---
+++ game.js
@@ -0,0 +1,16 @@
+const firstRandomNum = Math.floor(Math.random()*6) + 1
+const firstDiceImage ='assets/dice' + firstRandomNum + '.png';
+document.querySelectorAll('img')[0].setAttribute('src',firstDiceImage);
+
+const secondRandomNum = Math.floor(Math.random()*6) + 1
+const secondDiceImage ='assets/dice' + secondRandomNum + '.png';
+document.querySelectorAll('img')[0].setAttribute('src',secondDiceImage);
+
+if(firstRandomNum < secondRandomNum){
+    document.querySelector('h1').innerHTML = ' Winner is Player 2 !';
+}else if(firstRandomNum > secondRandomNum){
+    document.querySelector('h1').innerHTML = ' Winner is Player 1 !';
+}
+else {
+    document.querySelector('h1').innerHTML = ' Draw!!';
+}
